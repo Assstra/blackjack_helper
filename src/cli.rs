8d1bb767f6d24188f75d7ec1631587222229774d
Adrien Raimbault <adrien.raimbault@etu.umontepllier.fr>
2025-03-18 10:31:44 +0100

    misc: add user input function

--- src/cli.rs
+++ src/cli.rs
@@ -20,3 +20,45 @@ pub fn input_cli() -> u8 {
     };
     input
 }
+
+pub fn hit_or_stand_input() -> String {
+    let input = loop {
+        let mut s: String = String::new();
+        stdin()
+            .read_line(&mut s)
+            .expect("Did not enter a correct string");
+
+        let input = s.trim().parse::<String>();
+        match input {
+            Ok(input) => {
+                println!("You entered {}", input);
+                break input;
+            }
+            Err(..) => {
+                println!("Please enter a string!");
+            }
+        };
+    };
+    input
+}
+
+pub fn split_input() -> String {
+    let input = loop {
+        let mut s: String = String::new();
+        stdin()
+            .read_line(&mut s)
+            .expect("Did not enter a correct string");
+
+        let input = s.trim().parse::<String>();
+        match input {
+            Ok(input) => {
+                println!("You entered {}", input);
+                break input;
+            }
+            Err(..) => {
+                println!("Please enter a string!");
+            }
+        };
+    };
+    input
+}
